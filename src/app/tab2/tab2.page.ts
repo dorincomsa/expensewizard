aa0334804949433ea87d34905e181cd25401262e
Dorin Comsa <dorin.comsa@tss-yonder.com>
2024-05-27 14:22:13 +0300

    working

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Service } from '../services/Service';
+import { Conversation } from '../domain/conversation';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  question = ''
+  conversations: Conversation[] = [];
+
+  constructor(private service: Service){}
+
+  ngOnInit(): void {
+    this.service.getConvesations().subscribe(conversations => {
+      this.conversations = conversations.reverse().map(this.mapConversation)
+    })
+  }
+
+  onSubmit(): void {
+    this.service.addConversation(this.question).subscribe(response => {
+      this.conversations.unshift(response)
+    })
+    this.question = ''
+  }
+
+  stringify(resp: any): string{
+    return resp
+  }
+
+  mapConversation(conversation: Conversation): Conversation{
+    console.log(conversation)
+    return conversation;
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Service } from '../services/Service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  protected categories = this.service.categories
+  protected name: string = '';
+  protected category?: string = undefined;
+  protected value?: number = undefined;
+  protected date: string = new Date().toISOString();
 
-  constructor() {}
+  constructor(private service: Service) {}
 
+  addExpense(): void {
+    if(this.name && this.category && this.value && this.date){
+      this.service.addExpense(this.name, this.category, this.value, this.date).subscribe(() => {
+        console.log('added',this.name, this.category, this.value, this.date)
+      })
+    }
+    this.name = '';
+    this.category = undefined;
+    this.value = undefined;
+    this.date = new Date().toISOString();
+  }
 }
